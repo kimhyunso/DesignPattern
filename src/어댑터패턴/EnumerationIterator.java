7d68f83b49879d8e81c4bb6118c65f802e0ab768
kimhyunso <gustn9515@gmail.com>
2024-04-09 09:33:37 +0900

    20240409 09:33

---
+++ src/어댑터패턴/EnumerationIterator.java
@@ -0,0 +1,29 @@
+package 어댑터패턴;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class EnumerationIterator implements Iterator<Object>{
+
+    private Enumeration<?> enumeration;
+
+    public EnumerationIterator(Enumeration<?> enumeration){
+        this.enumeration = enumeration;
+    }
+
+    @Override
+    public boolean hasNext(){
+        return enumeration.hasMoreElements();
+    }
+
+    @Override
+    public Object next(){
+        return enumeration.nextElement();
+    }
+
+    @Override
+    public void remove(){
+        throw new UnsupportedOperationException();
+    }
+
+}
